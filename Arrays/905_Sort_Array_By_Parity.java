9bee15a30f46cac916993347c39441c7f1dbd206
Sangamithra K <sangamithra200731@gmail.com>
2026-08-03 19:27:17 +0530

    Solved LeetCode 905 - Sort Array By Parity

---
+++ Arrays/905_Sort_Array_By_Parity.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] sortArrayByParity(int[] arr) {
+         int[] a=new int[arr.length];
+        int b=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]%2==0){
+                a[b]=arr[i];
+                b++;
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]%2!=0){
+                a[b]=arr[i];
+                b++;
+            }
+        }   
+        
+       return a; 
+    }
+}
